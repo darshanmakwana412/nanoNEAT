9a60d6c83805866de38da6b8a099729bf7a2b4c2
Darshan Makwana <darshanmakwana412@gmail.com>
2024-02-24 15:23:59 +0530

    utilities for plotting a genome

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+import matplotlib.pyplot as plt
+from utils import plot_edge, plot_square
 
 class Genome:
     """Base class for the Genome"""
@@ -30,5 +31,42 @@ class Genome:
             (3, 4, 0.6, 1, 11),
         ]
 
-    def phenotype(self):
-        pass
+    def plot(self) -> None:
+
+        fig, ax = plt.subplots(figsize=(10, 10))
+        ax.set_xlim(-1, 3)
+        ax.set_ylim(-1, 4)
+
+        # Node positions by layer
+        layers = {0: [], 1: [], 2: []}
+        for i, gene in enumerate(self.node_genes):
+            if gene == 0:  # Input
+                layers[0].append(i)
+            elif gene == 1:  # Hidden
+                layers[1].append(i)
+            else:  # Output
+                layers[2].append(i)
+
+        # Calculate positions
+        positions = {}
+        for layer, nodes in layers.items():
+            y_step = 4 / (len(nodes) + 1)
+            for i, node in enumerate(nodes):
+                positions[node] = (layer, (i+1) * y_step)
+
+        # Draw nodes with enhanced aesthetics
+        for node, pos in positions.items():
+            is_hidden = self.node_genes[node] == 1
+            plot_square(ax, pos, node, is_hidden=is_hidden)
+
+        # Draw connections with enhanced aesthetics
+        for src, dst, weight, enabled, _ in self.connect_genes:
+            start_pos = positions[src]
+            end_pos = positions[dst]
+            plot_edge(ax, start_pos, end_pos, weight, enabled)
+
+        plt.axis('off')
+        plt.savefig("plot.png")
+
+g1 = Genome()
+g1.plot()
---
+++ utils.py
@@ -0,0 +1,23 @@
+"""
+Some of the main utilities for plotting the genome
+"""
+
+import matplotlib.patches as patches
+
+def plot_square(ax, center, node, size=0.4, is_hidden=False):
+    color = "green" if is_hidden else "grey"
+    ax.add_patch(patches.Rectangle((center[0]-size/2, center[1]-size/2), size, size, fill=True, color=color, linewidth=0, linestyle='-', zorder=5))
+    ax.text(center[0], center[1], str(node), ha='center', va='center', weight='bold', color="black", fontsize=15, zorder=6)
+
+def plot_edge(ax, start, end, weight, enabled):
+    if enabled:
+        if weight > 0:
+            color = "blue"
+        else:
+            color = "red"
+        linestyle = '-'
+    else:
+        color = "grey"
+        linestyle = ':'
+    linewidth = abs(weight * 10) + 1
+    ax.annotate("", xy=end, xycoords='data', xytext=start, textcoords='data', arrowprops=dict(arrowstyle="->", color=color, linestyle=linestyle, linewidth=linewidth, zorder=3))
